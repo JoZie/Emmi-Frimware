631afafd74ba622733efff7cb9bdaa8e85be5b0b
Johannes Ziegenbalg <Johannes.Ziegenbalg@gmail.com>
2021-03-28 22:41:29 +0200

    disable tap-dance for quotes

--- Enni.ino
+++ Enni.ino
@@ -218,7 +218,7 @@ KEYMAPS(
 
    M(MACRO_ANY), Key_6, Key_7, Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
    Key_Enter,    Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-                 Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Tap(Quote),
+                 Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    OSL(LANG),    Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
    Key_RightControl, Key_LeftGui, Key_Backspace, Key_RightShift,
